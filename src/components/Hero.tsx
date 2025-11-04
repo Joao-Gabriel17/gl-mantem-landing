603e3ad0a4905896a84649086b4d100b63f3913c
Okamiu_u <95306833+Joao-Gabriel17@users.noreply.github.com>
2025-11-04 14:12:13 -0300

    Refactor headline spans for improved structure

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -27,23 +27,23 @@ export const Hero = () => {
             {/* Headline */}
             <h1 className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-extrabold leading-[1.1] text-textc-100">
               Ar-condicionado instalado com{" "}
-              <span
-                className="hl hl-brush"
-                style={{ ["--brush-img" as any]: `url(${brushUrl})` }}
-              >
-                <span className="hl-inner">pontualidade e</span>
+            
+              {/* sublinhado “pincel” */}
+              <span className="hl-block">
+                <span className="hl-text">pontualidade e</span>
+                <img src={brushUrl} alt="" aria-hidden className="hl-underline" />
               </span>{" "}
-              <span
-                className="hl hl-marker"
-                style={{ ["--marker-img" as any]: `url(${marker1Url})` }}
-              >
-                <span className="hl-inner">acabamento</span>
+            
+              {/* marcador v1 */}
+              <span className="hl-block">
+                <img src={marker1Url} alt="" aria-hidden className="hl-img" />
+                <span className="hl-text">acabamento</span>
               </span>{" "}
-              <span
-                className="hl hl-marker"
-                style={{ ["--marker-img" as any]: `url(${marker2Url})` }}
-              >
-                <span className="hl-inner">impecável</span>
+            
+              {/* marcador v2 */}
+              <span className="hl-block">
+                <img src={marker2Url} alt="" aria-hidden className="hl-img" />
+                <span className="hl-text">impecável</span>
               </span>
             </h1>
 
